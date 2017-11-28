4f5f7f948e27d8835167cec5587fca7f9e49c3e2
Niyaz <nyz.git@gmail.com>
2017-11-28 11:47:33 +0530

    Step 4 Displaying a list Done

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Niyaz' },
+  { id: 12, name: 'Rakesh' },
+  { id: 13, name: 'Pankaj' },
+  { id: 14, name: 'Manoj' },
+  { id: 15, name: 'Zaid' },
+  { id: 16, name: 'Mitali' },
+  { id: 17, name: 'Srinivas' },
+  { id: 18, name: 'Harsha' },
+  { id: 19, name: 'Vijay' },
+  { id: 20, name: 'Sofia' }
+];
